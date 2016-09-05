98cc3f2803e75297bfa4ca23f86c1eef3c301c21
i-n00b <samarthmathur@Samarths-MacBook-Pro.local>
2016-09-05 00:30:53 -0700

    Added comments and minor changes

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/*                          Index.js
+This file contains the scripts for handlind server side functionality. It integrates the
+express.js web developement framework and the MongoDB database with the server.
+
+It contains the functions to handle saving chat messages, usernames and timestamps to the database,
+and sending them to a new user, and adding and removing users as they connect and disconnect from
+the server.
+*/
+
+//add all required modules (express, http, socket.io, mondoDB)
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -5,9 +15,11 @@ var io = require('socket.io')(http);
 var mongoose = require ('mongoose');
 var userNames = [];
 
+//add support for handling static files
 app.use(express.static(__dirname));
-
 mongoose.Promise = global.Promise;
+
+//connect to mongoDB
 mongoose.connect('mongodb://localhost/chat', function(err){
   if (err){
     console.log(err);
@@ -16,45 +28,55 @@ mongoose.connect('mongodb://localhost/chat', function(err){
   }
 });
 
+//create a mongoDB schema
 var chatSchema = mongoose.Schema({
   user: String,
-  data1: String,
+  messageData: String,
   created: {type: Date, default: Date.now}
 });
 
+//constructor for the schema
 var Chat = mongoose.model('Message', chatSchema);
 
+//deliver the HTML file to the client side
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/main.html');
 });
 
 io.on('connection', function(socket){
+  //on establishing a connection, send the chats stored in DB to the user
   var query1= Chat.find({});
+  //sort the last 50 messages by time created
   query1.sort('-created').limit(50).exec(function(err, docs){
     if(err) throw err;
     console.log('Sending old messages');
     socket.emit('load old messages', docs);
   });
+
+  //when a new user joins, check if the username is unique and not NULL,and broadcast username to all
   socket.on('new user', function(data,callback){
     if (userNames.indexOf(data) != -1 || data == ''){
       callback(false);
     } else{
       callback(true);
       socket.username = data;
-      var sockID = socket.id;
+      var sockId = socket.id;
       userNames.push(socket.username);
-      io.to(sockID).emit('all usernames',userNames);
+      io.to(sockId).emit('all usernames',userNames);
       io.emit('username', socket.username);
     }
   });
+
+  //on getting a new message, save the message and broadcasr to all
   socket.on('chat message', function(msg){
-    var newMsg = new Chat({data1: msg, user: socket.username});
+    var newMsg = new Chat({messageData: msg, user: socket.username});
     newMsg.save(function(err){
       if(err) throw err;
-      io.emit('send message', {data1: msg, user: socket.username});
+      io.emit('send message', {messageData: msg, user: socket.username});
   });
 });
 
+  //when a user disconnects, remove name from array, and broadcast to everyone that user left
   socket.on('disconnect', function(data){
     if (!socket.username)
       return;
@@ -64,6 +86,7 @@ io.on('connection', function(socket){
 
 });
 
+//listen for any connection requests on port 3000
 http.listen(3000, function(){
   console.log('Chat Room listening on port 3000');
 });
--- public/JS/FSEChatClient.js
+++ public/JS/FSEChatClient.js
@@ -1,8 +1,22 @@
+/*                           FSEChatClient.js
+This file is simply an external javascript file for handling all Javascript functionality
+on the Client Side. Its contains the functions to manage the client side features such as
+loading old messages stored in the database, displaying chat messages received from server,
+and notifying if any user joins or disconnects from the chat
+
+It also contains the functions to send chat messages and the username of  a new user to
+the server.
+
+Along with this it has helping functions for the bove features.
+*/
+
+//Function to check if the Username is Alphanumeric
 function checkAlpha(){
   var $userN = $('#Username');
   var userNLast = $userN.val().slice(-1);
   var alphaN = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890";
   var pos = alphaN.indexOf(userNLast);
+  //If name is not alphanumeric, display an error message
   if (pos == -1){
     $('#UsernameError').html('Please enter a username with only alphanumeric values and spaces');
     setTimeout(function(){
@@ -11,28 +25,32 @@ function checkAlpha(){
     },1000);
   }
 }
+
+
 var socket = io();
 var $userName = $('#Username');
-var $usernameError = $('#UsernameError');
-var d = new Date();
-var dd = d.getDate();
-var mm = d.getMonth() + 1;
 
+//function to login a new user
 $('#form1').submit(function(e){
   e.preventDefault();
+  //if the callback function authenticates username, hide the login element and display chat element
   socket.emit('new user', $userName.val(),function(data){
     if(data){
       $('#loginPage').hide();
       $('#chatPage').show();
-    } else{
-      $usernameError.html('This username already taken');
+    }
+    //else display error message
+    else{
+      $('#UsernameError').html('This username already taken');
       setTimeout(function(){
-        $('#UsernameError').html('');
+        $('#UsernameError').html(''); //clear error display after waiting a second
       },1000);
     }
   });
-  $userName.val('');
-})
+  $userName.val(''); //clear username box
+});
+
+//function to send a chat message to the server
 $('#form2').submit(function(e){
   e.preventDefault();
   socket.emit('chat message', $('#m').val());
@@ -40,29 +58,42 @@ $('#form2').submit(function(e){
   return false;
 });
 
+//display all current online users in the chat to a new user
 socket.on('all usernames', function(msg){
   for (var i=0; i<msg.length - 1; i++){
     $('#messages').append('<span id = "joinName">' + msg[i] + ' joined</span>' );
   }
 });
+
+//display message to everyone that a user has joined
 socket.on('username', function(msg){
   $('#messages').append('<span id = "joinName">' + msg + ' has joined</span>' );
 });
+
+//display message to everyone that a user left
 socket.on('user disconnect',function(msg){
   $('#messages').append('<span id = "leftName">' + msg + ' has left</span>' );
 });
+
+//load the messasges on the database with timestamps and userIDs for a new user
 socket.on('load old messages', function(docs){
   for (var i = docs.length - 1; i >= 0; i--){
     var currDate = docs[i].created;
     var onlyDate = currDate.slice(0,10);
     var onlyTime = currDate.slice(11,16);
-    $('#messages').append('<span id = "NameTime"><span id = "username"><b>' + docs[i].user + '</b></span> <span id = "timestamp">' + onlyDate + '      ' + onlyTime + '</span></span><span id = "textm">' + docs[i].data1 + "</span>" );
+    $('#messages').append('<span id = "NameTime"><span id = "username"><b>' +
+    docs[i].user + '</b></span> <span id = "timestamp">' + onlyDate + '      ' +
+    onlyTime + '</span></span><span id = "textm">' + docs[i].messageData + "</span>" );
   }
 });
+
+//display a real time message with userID and timestamp to all users
 socket.on('send message', function(msg){
   var dd1 = new Date();
   var n = dd1.toISOString();
   var nonlyDate = n.slice(0,10);
   var nonlyTime = n.slice(11,16);
-  $('#messages').append('<span id = "NameTime1"><span id = "username1"><b>' + msg.user + '</b></span> <span id = "timestamp1">' + nonlyDate + '      ' + nonlyTime + '</span></span><span id = "textm1">' + msg.data1 + "</span>" );
+  $('#messages').append('<span id = "NameTime1"><span id = "username1"><b>' +
+  msg.user + '</b></span> <span id = "timestamp1">' + nonlyDate + '      ' +
+  nonlyTime + '</span></span><span id = "textm1">' + msg.messageData + "</span>" );
 });
